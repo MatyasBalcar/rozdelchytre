63136848b0eb264d7269bae066aba9dd732d0f7c
matyas <matyasbalcar0@gmail.com>
2023-09-09 18:08:27 +0200

    Add funcs

--- main.py
+++ main.py
@@ -5,14 +5,30 @@ users=[]
 class User:
     def __init__(self, name):
         self.name =  name
-        self.debt =0
-
+        self.debt =0#negative is good
+    #changes debt amount based on amount given
+    def changeDebt(self, amount):
+        self.debt+=amount
 #adds a user based on name
 def addUser(name):
     users.append(User(name))
 
 #add an expense
 def addExpense(payer, people, amount):
-    pass
+    #debt for the debtors
+    debt =  amount / (len(people))
+    #what the guy payed for the other people aka all - his share
+    payed= (amount - debt)*-1
+    payer.changeDebt(payed)
+    print(f"added expense to {payer.name} of {payed}")
+    #adding debt to all people but the payer
+    for user in users:
+        if user!=payer:
+            user.changeDebt(debt)
+addUser("bali")
+addUser("fofo")
 
+addExpense(users[0],users ,100)
+
+print(users[0].debt)
     
