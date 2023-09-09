1528359aef7cce622180fb8b9bb555b5af940eae
matyas <matyasbalcar0@gmail.com>
2023-09-09 18:23:18 +0200

    Add more info into debts

--- main.py
+++ main.py
@@ -6,9 +6,12 @@ class User:
     def __init__(self, name):
         self.name =  name
         self.debt =0#negative is good
+        self.debts_list= {}
     #changes debt amount based on amount given
     def changeDebt(self, amount):
         self.debt+=amount
+    def newDebt(self, amount, debtor):
+        self.debts_list[debtor]=(amount)
 #adds a user based on name
 def addUser(name):
     users.append(User(name))
@@ -20,11 +23,13 @@ def addExpense(payer, people, amount):
     #what the guy payed for the other people aka all - his share
     payed= (amount - debt)*-1
     payer.changeDebt(payed)
+
     print(f"added expense to {payer.name} of {payed}")
     #adding debt to all people but the payer
     for user in users:
         if user!=payer:
             user.changeDebt(debt)
+            user.newDebt(debt, payer.name)
 def showExpenses(people):
     for user in people:
         if user.debt >0:
@@ -33,6 +38,12 @@ def showExpenses(people):
             print(f"User {user.name} is owed {user.debt *-1}")
         else:
             print(f"User doesnt owe anything.")
+def showDebts(people):
+    for user in people:
+
+        print(f"{user.name}'s debts")
+        print(user.debts_list)
+
 ## TEST CODE ##
 addUser("bali")
 addUser("fofo")
@@ -42,3 +53,4 @@ addExpense(users[0],users ,100)
 print(users[0].debt)
 
 showExpenses(users)
+showDebts(users)
